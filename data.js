9427389b5809784166f5b80fec13ee6b0f974ca9
anhminhngo759 <traitimcuaanh,traitimcuamay@gmail.com>
2024-03-25 13:49:46 +0700

    bt midterm

---
+++ data.js
@@ -0,0 +1,197 @@
+var tour = [
+  {
+    id: "TO2785",
+    name: "Tour Châu Đốc 1N1Đ: Viếng Bà - Miếu Bà Chúa Xứ Núi Sam",
+    content: `NGÀY 01: MIẾU BÀ CHÚA XỨ - NÚI CẤM/RỪNG TRÀM TRÀ SƯ - TP.HCM (ĂN SÁNG, TRƯA)
+     04h00: Đến Châu Đốc, quý khách dừng chân nghỉ ngơi, vệ sinh cá nhân sau đó viếng Miếu Bà Chúa Xứ
+    Núi Sam nổi tiếng hiển linh, viếng Tây An Cổ Tự, Lăng Thoại Ngọc Hầu - Người có công khai mở đất 
+    An Giang.
+    06h30: Quý khách dùng điểm tâm sáng buffet (nếu trường hợp quá tải trong mùa cao điểm thì sẽ ăn theo dạng tô ly).Sau đó đoàn tham quan và mua sắm tại Chợ Tịnh Biên.
+    14h30: Đoàn khởi hành về TP.HCM.
+    Trên đường ghé trạm dừng chân mua sắm đặc sản Nem Lai Vung Đồng Tháp.
+Dự kiến khoảng 22h00 đoàn về đến TP.HCM, kết thúc chương trình. Hướng dẫn viên chia tay và hẹn gặp lại Quý khách trong những chuyến tour sau! 
+     `,
+    price: "790.000vnd",
+    timedi: "23/03/2024",
+    timeve: "24/03/2024",
+    image: "https://lh6.googleusercontent.com/bRI2oh6rYH7oeoZFsF5Hg6F0vHZ9j6Cg0NwEHMh_wI7plH-dmodxNcTPdzP0wWlDc8pb-tNnXRBB9rRp5xSqWKvUR3_n36S8RDFHGtyy_BvLMhE-Y8uZpV64U9E2pHWkUbBKODRd8wIzjlPfgrrbygY?dpr=2&w=675&fit=scale&auto=format&fm=pjpg"
+  },
+
+  {
+    id: "TO2786",
+    name: "Tour Limousine Cao Cấp Miền Tây 1N: Cà Mau - Cánh Đồng Quạt Gió",
+    content: `NGÀY 1: ĐẤT MŨI- BẠC LIÊU ( ĂN SÁNG, TRƯA)
+      Buổi sáng: 06h00: Đoàn dùng bữa sáng. Quý khách đặt chân lên mảnh đất “Cực nam của Tổ quốc”, ngắm mũi Cà Mau với khu rừng ngập mặn lớn thứ hai trên thế giới.
+      Đoàn chụp hình lưu niệm tại Cột mốc tọa độ quốc gia, Panô biểu tượng mũi Cà Mau và Cột mốc Đường Hồ Chí Minh - điểm cuối Cà Mau Km2436 là điểm 
+      đến du lịch đánh dấu "điểm cuối cùng" trên chuyến hành trình trải dài cả quốc gia, đi qua 28 tỉnh,
+      thành phố với chiều dài 3.183km.Sau khi check in, lưu niệm tại cột mốc. 
+      Công ty sẽ trao Giấy chứng nhận hoàn thành hành trình chinh phục Cực Nam Tổ Quốc Việt Nam  Mũi Cà Mau
+       cho quý khách.
+      09h00: Khởi hành về Cà Mau.
+      12h00: Đoàn dùng cơm trưa với những món đặc sản bản xứ Tây Nam Bộ tại Đất Mũi.
+      17h00: Xe đưa đoàn dạo quanh một vòng tham quan, ngắm cảnh thành phố Bạc Liêu. Quý khách tham quan và chụp hình tại Nhà công tử Bạc Liêu, một trong những ngôi nhà lớn nhất miền tây nam bộ thời bấy giờ. Tham quan nhà máy phong điện (cánh đồng quạt gió, điểm sống ảo đang cuốn hút lữ khách).
+      `,
+    price: "1.000.000vnd",
+    timedi: "15/03/2024",
+    timeve: "15/03/2024",
+    image: "https://cdn2.ivivu.com/2023/03/31/13/ivivu-chua-som-rong-soc-trang-750x390.gif"
+  },
+
+  {
+    id: "TO2787",
+    name: "Tour Hà Nội Trong Ngày: Tham quan Chùa Hương",
+    content: `NGÀY 1: CHÙA HƯƠNG (ĂN TRƯA)
+      8h30: Xe và hướng dẫn viên đón quý khách tại Khu Vực Phố Cổ Hoặc Nhà Hát Lớn Hà Nội để khởi hành đi Chùa Hương
+      10h15: Đoàn tới bến Đục và đi bộ lên bến Đục.
+      10h30: Quý khách đi thuyền dọc theo suối Yến (4 km), thưởng thức cảnh đẹp thơ mộng dọc 2 bên dòng sông và dòng nước trong mát với những thảm tảo tuyệt đẹp. Quý khách đến bến Thiên Trù và thăm quan chùa Thiên Trù.
+      12h15: Quý khách ăn trưa tại nhà hàng địa phương.
+      13h15: Quý khách tham quan toàn cảnh chùa Hương leo núi (5km) hoặc đi cáp. Sau đó quý khách đi thuyền dọc theo suối Yên quay trở lại bến Đục.
+      16h30: Quý khách lên xe trở về Hà Nội.
+      18h30: Về đến Hà Nội. Kết thúc tour.`,
+    price: "850.000vnd",
+    timedi: "20/03/2024",
+    timeve: "20/03/2024",
+    image: "https://cdn2.ivivu.com/2022/10/19/15/ivivu-chua-huong-gate.gif"
+  },
+];
+
+function Show() {
+  var html = "";
+  var stt = 0;
+  for (var i = 0; i < tour.length; i++) {
+    ;
+    stt++;
+    html += "<tr>";
+    html += "<td>" + stt + "</td>";
+    html += "<td>" + tour[i].id + "</td>";
+    html += "<td>" + tour[i].name + "</td>";
+    html += "<td><img src='" + tour[i].image + "' alt='Travel Image' style='width: 200px; height: 200px;'></td>";
+    // html += "<td>"+arr[i].image+"</td>";
+    html += "<td>" + tour[i].content;
+    html += "<td>" + tour[i].price + "</td>";
+    html += "<td>" + tour[i].timedi + "</td>";
+    html += "<td>" + tour[i].timeve + "</td>";
+    html += "<td>"
+    html += '<button class="btn btn-primary bt1" onclick="editTour(' + i + ')">Edit</button>'
+    html += '<button class="btn btn-primary bt1" onclick="deleteTour(' + i + ')">Delete</button>'
+    html += '<button class="btn btn-primary " onclick="detailTour(' + i + ')">Detail</button>'
+    html += "</td>"
+    html += "</tr>";
+  }
+  document.getElementById("tbl").innerHTML = html;
+}
+
+function createNewTour() {
+  var a = {
+    id: document.getElementById('id').value,
+    name: document.getElementById('name').value,
+    image: document.getElementById('image').value,
+    content: document.getElementById('content').value,
+    price: document.getElementById('price').value,
+    timedi: document.getElementById('timedi').value,
+    timeve: document.getElementById('timeve').value,
+  };
+  console.log(a);
+  tour.push(a);
+  Show();
+}
+
+function Reset() {
+  document.getElementById('id').value = '';
+  document.getElementById('name').value = '';
+  document.getElementById('image').value = '';
+  document.getElementById('content').value = '';
+  document.getElementById('price').value = '';
+  document.getElementById("timedi").value = '';
+  document.getElementById("timeve").value = '';
+}
+
+function editTour(index) {
+  document.getElementById("editId").value = tour[index].id;
+  document.getElementById("editName").value = tour[index].name;
+  document.getElementById("editContent").value = tour[index].content;
+  document.getElementById("editImage").value = tour[index].image;
+  document.getElementById("editPrice").value = tour[index].price;
+  document.getElementById("editTimedi").value = tour[index].timedi;
+  document.getElementById("editTimeve").value = tour[index].timeve;
+
+  var update = "<input type='button' class='btn btn-success' value='Update Tour' onclick='updateTour(" + index + ")' />";
+  document.getElementById("div-update").innerHTML = update;
+  document.getElementById("editForm").style.display = "block";
+}
+
+function updateTour(index) {
+  var newId = document.getElementById("editId").value;
+  var newName = document.getElementById("editName").value;
+  var newImage = document.getElementById("editImage").value;
+  var newContext = document.getElementById("editContent").value;
+  var newPrice = document.getElementById("editPrice").value;
+  var newTimedi = document.getElementById("editTimedi").value;
+  var newTimeve = document.getElementById("editTimeve").value;
+  var indexToUpdate = index;
+
+  console.log(index)
+  tour[indexToUpdate].id = newId;
+  tour[indexToUpdate].name = newName;
+  tour[indexToUpdate].image = newImage;
+  tour[indexToUpdate].content = newContext;
+  tour[indexToUpdate].price = newPrice;
+  tour[indexToUpdate].timedi = newTimedi;
+  tour[indexToUpdate].timeve = newTimeve;
+  document.getElementById("editForm").style.display = "none";
+  Show();
+}
+function deleteTour(index) {
+  tour.splice(index, 1);
+  Show();
+}
+
+function detailTour(index) {
+  document.getElementById("detailId").textContent = tour[index].id;
+  document.getElementById("detailName").textContent = tour[index].name;
+
+  document.getElementById("detailImage").src = tour[index].image;
+  const image = document.querySelector("#detailImage");
+  image.src = "" + tour[index].image + "";
+  document.getElementById("detailContent").textContent = tour[index].content;
+  document.getElementById("detailPrice").textContent = tour[index].price;
+  document.getElementById("detailStartTime").textContent = tour[index].timedi;
+  document.getElementById("detailEndTime").textContent = tour[index].timeve;
+
+  document.getElementById("detailForm").style.display = "block";
+}
+
+function searchName() {
+  var searchName = document.getElementById("Search").value;
+  console.log(searchName);
+  //loc name
+  var result = tour.filter(obj => {
+    return obj.name === searchName
+  })
+
+  console.log('result: ', result);
+
+  if (result.length > 0) {
+    // chuyen tu mang sang object
+    var attribute = result.reduce((acc, obj) => {
+      acc = obj
+      return acc
+    });
+    console.log('attribute: ', attribute);
+    document.getElementById("searchName").textContent = attribute.name;
+    document.getElementById("searchImage").src = attribute.image;
+    const image = document.querySelector("#searchImage");
+    image.src = "" + attribute.image + "";
+    document.getElementById("searchPrice").textContent = attribute.price;
+    document.getElementById("searchStartTime").textContent = attribute.timedi;
+    document.getElementById("searchEndTime").textContent = attribute.timeve;
+
+    document.getElementById("searchForm").style.display = "block";
+    document.getElementById("error").style.display = "none";
+  } else {
+    // Hiển thị thông báo "Không tìm thấy sản phẩm"
+    document.getElementById("searchForm").style.display = "none";
+    document.getElementById("error").textContent = "Không tìm thấy sản phẩm!";
+    document.getElementById("error").style.display = "block";
+  }
+}
